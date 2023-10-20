9a14e64a088a04da078da59392c379304406918c
paul-nguyen96 <paul121596@icloud.com>
2023-10-20 17:22:21 -0500

    debug

--- script.js
+++ script.js
@@ -1,52 +1,43 @@
 let currentQuestionIndex = 0;
 const questions = [
-    {
-      key: 1,
-      question: "Who was the 16th President of the United States?",
-      choices: ["wrong", "wrong", "wrong", "correct"],
-      correctAnswer: "correct",
-    },
-    {
-      key: 2,
-      question: "Who was the 16th President of the United States?",
-      choices: ["correct", "wrong", "wrong", "wrong"],
-      correctAnswer: "correct",
-    },
-  ];
+  {
+    key: 1,
+    question: "Who was the 16th President of the United States?",
+    choices: ["wrong", "wrong", "wrong", "correct"],
+    correctAnswer: "correct",
+  },
+  {
+    key: 2,
+    question: "Who was the 16th President of the United States?",
+    choices: ["correct", "wrong", "wrong", "wrong"],
+    correctAnswer: "correct",
+  },
+];
 
+const choices = document.querySelectorAll(".child-container");
 
-  const choices = document.querySelectorAll('.child-container');
-  
-  const displayQuestion = (question) => {
-      Array.from(choices).forEach((choiceElement, index) => {
-  
-         choiceElement.textContent = question.choices[index];
-  
-         choiceElement.addEventListener('click', () => {
-              if (question.choices[index] === question.correctAnswer) { 
-                  console.log("Correct!");
-              } else {
-                  console.log("Incorrect!");
-              }
-  
-              currentQuestionIndex++;
-              if (currentQuestionIndex < questions.length) {
-                  displayQuestion(questions[currentQuestionIndex]);
-              } else {
-                  console.log("Quiz completed!");
-              }
-          });
-      });
-  };
-  
-  displayQuestion(questions[currentQuestionIndex]);
+const displayQuestion = (question) => {
+  Array.from(choices).forEach((choiceElement, index) => {
+    choiceElement.textContent = question.choices[index];
+
+    choiceElement.addEventListener("click", () => {
+      if (question.choices[index] === question.correctAnswer) {
+        console.log("Correct!");
+      } else {
+        console.log("Incorrect!");
+      }
+      currentQuestionIndex > questions.length ? console.log('Quiz Completed') : console.log('Still in Progress')
+    });
+  });
+};
+
+displayQuestion(questions[currentQuestionIndex]);
 
 questions.map((element) => {
   console.log(element.key);
   console.log(element.choices);
-  const correctAnswer = element.correctAnswer
+  const correctAnswer = element.correctAnswer;
   for (let choice of element.choices) {
     choice == correctAnswer ? console.log("correct") : console.log("incorrect");
   }
 });
-
