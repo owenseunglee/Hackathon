01ea02cd645201fdeb5087eb92fe749d66cbf16e
paul-nguyen96 <paul121596@icloud.com>
2023-10-21 17:56:12 -0500

    remove localstorage on homepage

--- script.js
+++ script.js
@@ -173,6 +173,11 @@ topics.forEach((topic) => {
   });
 });
 
+
+if (window.location.pathname.includes("homepage.html")){
+  localStorage.removeItem('current_topic');
+}
+
 if (window.location.pathname.includes("gameboard.html")) {
   const current_topic = JSON.parse(localStorage.getItem('current_topic'));
   const restartBtn = document.getElementById("restart-btn");
