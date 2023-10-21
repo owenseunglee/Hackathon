f4e34af0581b27df01815647753b9c32a429c951
paul-nguyen96 <paul121596@icloud.com>
2023-10-21 17:51:19 -0500

    updated category script

--- script.js
+++ script.js
@@ -1,170 +1,273 @@
-const questions = [
+const cs_questions = [
   {
     index: 1,
-    question: "Question 1",
+    topic: "Computer Science",
+    question: "CS Question 1",
     choices: ["wrong", "wrong", "wrong", "correct"],
     correctAnswer: "correct",
   },
   {
     index: 2,
+    topic: "Computer Science",
     question: "Question 2",
     choices: ["correct", "wrong", "wrong", "wrong"],
     correctAnswer: "correct",
   },
   {
     index: 3,
+    topic: "Computer Science",
     question: "Question 3",
     choices: ["wrong", "correct", "wrong", "wrong"],
     correctAnswer: "correct",
   },
   {
     index: 4,
+    topic: "Computer Science",
     question: "Question 4",
     choices: ["wrong", "wrong", "correct", "wrong"],
     correctAnswer: "correct",
   },
   {
     index: 5,
+    topic: "Computer Science",
     question: "Question 5",
     choices: ["correct", "wrong", "wrong", "wrong"],
     correctAnswer: "correct",
   },
 ];
 
-const restartBtn = document.getElementById("restart-btn");
-const choices = document.querySelectorAll(".child-container");
-const question = document.getElementById("question");
-const gameEndPopup = document.querySelector(".game-end-popup");
-const overlay = document.querySelector(".overlay");
-let currentQuestionIndex = 0;
-let score = 0;
+const history_questions = [
+  {
+    index: 1,
+    topic: "History",
+    question: "History Question 1",
+    choices: ["wrong", "wrong", "wrong", "correct"],
+    correctAnswer: "correct",
+  },
+  {
+    index: 2,
+    topic: "History",
+    question: "Question 2",
+    choices: ["correct", "wrong", "wrong", "wrong"],
+    correctAnswer: "correct",
+  },
+  {
+    index: 3,
+    topic: "History",
+    question: "Question 3",
+    choices: ["wrong", "correct", "wrong", "wrong"],
+    correctAnswer: "correct",
+  },
+  {
+    index: 4,
+    topic: "History",
+    question: "Question 4",
+    choices: ["wrong", "wrong", "correct", "wrong"],
+    correctAnswer: "correct",
+  },
+  {
+    index: 5,
+    topic: "History",
+    question: "Question 5",
+    choices: ["correct", "wrong", "wrong", "wrong"],
+    correctAnswer: "correct",
+  },
+];
 
-let displayScore = document.createElement("div");
-displayScore.className = "display-score";
-displayScore.textContent = score;
-document.body.appendChild(displayScore);
+const vocab_questions = [
+  {
+    index: 1,
+    topic: "Vocabulary",
+    question: "Vocab Question 1",
+    choices: ["wrong", "wrong", "wrong", "correct"],
+    correctAnswer: "correct",
+  },
+  {
+    index: 2,
+    topic: "Vocabulary",
+    question: "Question 2",
+    choices: ["correct", "wrong", "wrong", "wrong"],
+    correctAnswer: "correct",
+  },
+  {
+    index: 3,
+    topic: "Vocabulary",
+    question: "Question 3",
+    choices: ["wrong", "correct", "wrong", "wrong"],
+    correctAnswer: "correct",
+  },
+  {
+    index: 4,
+    topic: "Vocabulary",
+    question: "Question 4",
+    choices: ["wrong", "wrong", "correct", "wrong"],
+    correctAnswer: "correct",
+  },
+  {
+    index: 5,
+    topic: "Vocabulary",
+    question: "Question 5",
+    choices: ["correct", "wrong", "wrong", "wrong"],
+    correctAnswer: "correct",
+  },
+];
 
-let activeThumbsUp = document.querySelector(".thumbs-up-active");
-let activeThumbsDown = document.querySelector(".thumbs-down-active");
-const restartLevel = document.querySelector('.restart-icon')
+const math_questions = [
+  {
+    index: 1,
+    topic: "Math",
+    question: "Math Question 1",
+    choices: ["wrong", "wrong", "wrong", "correct"],
+    correctAnswer: "correct",
+  },
+  {
+    index: 2,
+    topic: "Math",
+    question: "Question 2",
+    choices: ["correct", "wrong", "wrong", "wrong"],
+    correctAnswer: "correct",
+  },
+  {
+    index: 3,
+    topic: "Math",
+    question: "Question 3",
+    choices: ["wrong", "correct", "wrong", "wrong"],
+    correctAnswer: "correct",
+  },
+  {
+    index: 4,
+    topic: "Math",
+    question: "Question 4",
+    choices: ["wrong", "wrong", "correct", "wrong"],
+    correctAnswer: "correct",
+  },
+  {
+    index: 5,
+    topic: "Math",
+    question: "Question 5",
+    choices: ["correct", "wrong", "wrong", "wrong"],
+    correctAnswer: "correct",
+  },
+];
 
+const categories = [
+  cs_questions,
+  history_questions,
+  vocab_questions,
+  math_questions,
+];
 
-let timer;
-let timerElement = document.getElementById('timer');
-
-function startTimer(){
-  var sec = 10;
-  timer = setInterval(()=>{
-    timerElement.innerHTML = sec;
-    if (sec == 0) {
-      clearInterval(timer);
-      if (currentQuestionIndex < questions.length) {
-        startTimer();
-        currentQuestionIndex++
-        updateQuestion()
+let current_topic = null;
+const topics = document.querySelectorAll(".topic-container");
+topics.forEach((topic) => {
+  topic.addEventListener("click", () => {
+    const topicTitle = topic.querySelector(".topic-title").textContent;
+    for (let category of categories) {
+      if (topicTitle.trim().toLowerCase() == category[0].topic.trim().toLowerCase()) {
+        current_topic = category
+        console.log(topicTitle)
+        localStorage.setItem('current_topic', JSON.stringify(current_topic));
+        window.location.href = 'gameboard.html';
       }
     }
-    sec--;
-  }, 1000)
-}
-
-function correctAnswer() {
-  score++;
-  currentQuestionIndex++;
-  displayScore.textContent = score;
-  activeThumbsUp.style.transform = "translateX(15vw)";
-  clearInterval(timer);
-  updateQuestion();
-  startTimer();
-
-  setTimeout(function () {
-    activeThumbsUp.style.transform = "translateX(0vw)";
-  }, 1000);
-}
-
-function wrongAnswer() {
-  currentQuestionIndex++;
-  activeThumbsDown.style.transform = "translateX(0vw)";
-  clearInterval(timer);
-  updateQuestion();
-  startTimer();
-
-  setTimeout(function () {
-    activeThumbsDown.style.transform = "translateX(50vw)";
-  }, 1000);
-
-}
-
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-}
-
-function restartGame() {
-  gameEndPopup.classList.add("hide");
-  overlay.classList.add("hide");
-  score = 0;
-  displayScore.textContent = score;
-  currentQuestionIndex = 0;
-  shuffleArray(questions);
-  startTimer();
-  updateQuestion()
-}
-
-function resetLevel() {
-  score = 0;
-  displayScore.textContent = score;
-  currentQuestionIndex = 0;
-  updateQuestion();
-}
-
-function gameFinished() {
-  clearInterval(timer);
-  gameEndPopup.classList.remove("hide");
-  overlay.classList.remove("hide");
-}
-
-function updateQuestion() {
-  if (currentQuestionIndex < questions.length) {
-    Array.from(choices).forEach((choiceElement, index) => {
-      choiceElement.textContent =
-        questions[currentQuestionIndex].choices[index];
-      question.textContent = questions[currentQuestionIndex].question;
-    });
-  } else {
-    gameFinished();
-  }
-}
-
-restartBtn.addEventListener("click", function () {
-  restartGame();
-  updateQuestion();
-});
-
-Array.from(choices).forEach((choiceElement, index) => {
-  choiceElement.addEventListener("click", () => {
-    if (
-      questions[currentQuestionIndex].choices[index] ===
-      questions[currentQuestionIndex].correctAnswer
-    ) {
-      correctAnswer();
-    } else {
-      let correctChoice = Array.from(choices).find((choice) =>
-        choice.textContent.includes(questions[currentQuestionIndex].correctAnswer)
-      );
-
-      console.log(correctChoice);
-      wrongAnswer();
-    }
-    updateQuestion();
   });
 });
 
-startTimer();
-updateQuestion();
+if (window.location.pathname.includes("gameboard.html")) {
+  const current_topic = JSON.parse(localStorage.getItem('current_topic'));
+  const restartBtn = document.getElementById("restart-btn");
+  const choices = document.querySelectorAll(".child-container");
+  const question = document.getElementById("question");
+  const gameEndPopup = document.querySelector(".game-end-popup");
+  const overlay = document.querySelector(".overlay");
+  let currentQuestionIndex = 0;
+  let score = 0;
 
-restartLevel.addEventListener('click', () => {
-  resetLevel()
-})
+  let displayScore = document.createElement("div");
+  displayScore.className = "display-score";
+  displayScore.textContent = score;
+  document.body.appendChild(displayScore);
+
+  let activeThumbsUp = document.querySelector(".thumbs-up-active");
+  let activeThumbsDown = document.querySelector(".thumbs-down-active");
+
+  function correctAnswer() {
+    score++;
+    currentQuestionIndex++;
+    displayScore.textContent = score;
+    activeThumbsUp.style.transform = "translateX(15vw)";
+    setTimeout(function () {
+      activeThumbsUp.style.transform = "translateX(0vw)";
+    }, 1000);
+  }
+
+  function wrongAnswer() {
+    currentQuestionIndex++;
+    activeThumbsDown.style.transform = "translateX(0vw)";
+    setTimeout(function () {
+      activeThumbsDown.style.transform = "translateX(50vw)";
+    }, 1000);
+  }
+
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  function restartGame() {
+    gameEndPopup.classList.add("hide");
+    overlay.classList.add("hide");
+    score = 0;
+    displayScore.textContent = score;
+    currentQuestionIndex = 0;
+    shuffleArray(current_topic);
+    updateQuestion();
+  }
+
+  function gameFinished() {
+    gameEndPopup.classList.remove("hide");
+    overlay.classList.remove("hide");
+  }
+
+  function updateQuestion() {
+    if (currentQuestionIndex < current_topic.length) {
+      Array.from(choices).forEach((choiceElement, index) => {
+        choiceElement.textContent =
+          current_topic[currentQuestionIndex].choices[index];
+        question.textContent = current_topic[currentQuestionIndex].question;
+      });
+    } else {
+      gameFinished();
+    }
+  }
+
+  restartBtn.addEventListener("click", function () {
+    restartGame();
+    updateQuestion();
+  });
+
+  Array.from(choices).forEach((choiceElement, index) => {
+    choiceElement.addEventListener("click", () => {
+      if (
+        current_topic[currentQuestionIndex].choices[index] ===
+        current_topic[currentQuestionIndex].correctAnswer
+      ) {
+        correctAnswer();
+      } else {
+        let correctChoice = Array.from(choices).find((choice) =>
+          choice.textContent.includes(
+            current_topic[currentQuestionIndex].correctAnswer
+          )
+        );
+        console.log(correctChoice);
+        wrongAnswer();
+      }
+      updateQuestion();
+    });
+  });
+
+  // Reloads page for current question
+  updateQuestion();
+}
